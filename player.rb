51f17001460c386673d7dd9213daf4e431008427
Rupa Dhillon <rupa211984@gmail.com>
2019-01-14 19:38:23 -0800

    Get valid input from player, return action & coords in array

---
+++ player.rb
@@ -0,0 +1,55 @@
+# Get player input and validate, e.g. f0,0 r0,1
+# Player can flag (f) or reveal(r), coords must be on board
+class Player
+  ACTIONS = 'fr'
+
+  def initialize(rows, cols)
+    @rows_range = (0...rows).to_a
+    @cols_range = (0...cols).to_a
+  end
+
+  def get_input
+    input = format_input(gets.chomp)
+    until valid_input?(input)
+      puts "Please provide a valid action (#{ACTIONS}) and coordinates, e.g. r0,0 or f1,4"
+      input = format_input(gets.chomp)
+    end
+    input_to_array(input)
+  end
+
+  private
+
+  def format_input(string)
+    string.downcase.delete(' ')
+  end
+
+  def parse_input(string)
+    action = /[a-z]*/.match(string)
+    rest = /\d+.*/.match(string)
+    [action, rest]
+  end
+
+  def parse_coords(match_data)
+    match_data[0].split(',').map(&:to_i)
+  end
+
+  def input_to_array(string)
+    action, rest = parse_input(string)
+    coords = parse_coords(rest)
+    [action[0], coords]
+  end
+
+  def valid_input?(string)
+    action, rest = parse_input(string)
+    return false if [action, rest].any?(&:nil?)
+    x, y = parse_coords(rest)
+    return true if @rows_range.include?(x) && @cols_range.include?(y) && 
+                   ACTIONS.include?(action[0])
+    false
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  player = Player.new(9, 9)
+  p player.get_input
+end
