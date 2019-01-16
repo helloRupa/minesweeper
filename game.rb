00109862b9f90e460d1baefa00b7aa5269bd4283
Rupa Dhillon <rupa211984@gmail.com>
2019-01-15 18:08:41 -0800

    Add run & supporting methods, refactor init

--- game.rb
+++ game.rb
@@ -20,9 +20,12 @@ class Game
 
   def initialize
     welcome_message
+    @game_over = false
     mode = get_mode
     @mines = num_mines(mode)
-    @board = Board.new(@mines, board_size(mode))
+    board_size = board_size(mode)
+    @board = Board.new(@mines, board_size)
+    @player = Player.new(board_size)
   end
 
   def welcome_message
@@ -67,9 +70,49 @@ class Game
     @board.render
     puts
   end
+
+  def set_game_over(coords)
+    y, x = coords
+    tile = @board.get_tile(y, x)
+    @game_over = @board.mine?(tile)
+  end
+
+  def game_won?
+    @board.complete?
+  end
+
+  def turn_msg
+    puts 'Please enter an action(f or r) and coordinates, e.g. f0,0 or r1,1'
+    print '> '
+  end
+
+  def turn(is_first)
+    render
+    turn_msg
+    action, coords = @player.get_input
+    @board.populate(coords) if is_first
+    action == 'r' ? @board.reveal(coords) : @board.flag(coords)
+    set_game_over(coords)
+  end
+
+  def game_over_msg
+    puts 'Sorry you lose. Better luck next time.'
+  end
+
+  def win_msg
+    puts 'Congratulations! You win!'
+  end
+
+  def run
+    turn(true)
+    turn(false) until @game_over || game_won?
+    @game_over ? game_over_msg : win_msg
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
   game = Game.new
-  game.render
+  game.run
+  # game.render
+  # game.game_over?([0,0])
 end
