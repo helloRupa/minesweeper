ed385190261d2b7ce754492e5f25b91be6883d43
Rupa Dhillon <rupa211984@gmail.com>
2019-01-16 19:06:09 -0800

    Start and stop timer, get final time

--- leaderboard.rb
+++ leaderboard.rb
@@ -1,3 +1,21 @@
 # Time games, save leaderboard to text, print leaderboard
 class Leaderboard
+  def self.start_timer
+    @@time = Time.now
+  end
+
+  def self.stop_time
+    (Time.now - @@time).round(3)
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  Leaderboard.start_timer
+  
+  input = nil
+  until input == 'y'
+    print '> '
+    input = gets.chomp
+  end
+  p Leaderboard.stop_time
 end
